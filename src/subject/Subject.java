184d9ad3ee8ba04525d18b62538b1fc1ecfdc3a2
BasovMaxym <maksimka.basov2000@gmail.com>
2020-04-15 00:23:57 +0300

    Initial commit

---
+++ src/subject/Subject.java
@@ -0,0 +1,75 @@
+package subject;
+
+import java.util.Random;
+
+public class Subject
+{
+
+    public Subject()
+    {
+
+
+
+    }
+
+    public Subject(String nameSubject)
+    {
+
+        Random number = new Random();
+
+        this.nameSubject = nameSubject;
+
+        this.mark = 2 + number.nextDouble() * 3;
+
+        this.passed = passedSubject();
+
+    }
+
+    private boolean passedSubject()
+    {
+
+        if(this.mark <= 2)
+        {
+
+            return false;
+
+        }
+        else return true;
+
+    }
+
+    public double getMark()
+    {
+
+        return this.mark;
+
+    }
+
+    public boolean getPassed()
+    {
+
+        return this.passed;
+
+    }
+
+    public String getNameSubject()
+    {
+
+        return this.nameSubject;
+
+    }
+
+    public void setNameSubject(String nameSubject)
+    {
+
+        this.nameSubject = nameSubject;
+
+    }
+
+    private boolean passed;
+
+    private double mark;
+
+    private String nameSubject;
+
+}
